fc056d35b1adebb5c19739fd975338809d52bf30
SegFault42 <rabougue@student.42.fr>
2017-07-14 18:22:14 +0200

    i think it's ok

--- source_otool/main.c
+++ source_otool/main.c
@@ -1,16 +1,22 @@
 #include "otool.h"
 
-static void	o_tool(char *ptr)
+static void	o_tool(char *ptr, char *argv)
 {
 	uint32_t	magic_number;
 
 	magic_number = *(uint32_t *)(void *)ptr;
 	if (magic_number == MH_MAGIC_64)
+	{
+		ft_dprintf(1, "%s:\n(__TEXT,__text) section\n", argv);
 		handle_64(ptr);
+	}
 	else if (magic_number == MH_MAGIC)
+	{
+		ft_dprintf(1, "%s:\n(__TEXT,__text) section\n", argv);
 		handle_32(ptr);
+	}
 	else
-		ft_dprintf(2, RED"NOT SUPPORTED YET :(\n"END);
+		ft_dprintf(2, RED"%s NOT SUPPORTED YET :(\n"END, argv);
 }
 
 int	main(int argc, char **argv)
@@ -26,8 +32,7 @@ int	main(int argc, char **argv)
 		if (!setup.ptr)
 			continue ;
 			/*return (EXIT_FAILURE);*/
-		ft_dprintf(1, "%s:\n(__TEXT,__text) section\n", argv[i]);
-		o_tool(setup.ptr);
+		o_tool(setup.ptr, argv[i]);
 		if (setup_unset(argc, argv[i], &setup, 'u') == (char *)-1)
 			return (EXIT_FAILURE);
 	}
